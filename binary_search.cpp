ca1a22e7463130b37589dec3c665a591c6d6d611
tczf <tczf1128@gmail.com>
2014-09-04 22:11:43 +0800

    Add searchInsert and searchRotate

--- binary_search.cpp
+++ binary_search.cpp
@@ -64,5 +64,82 @@ int find_upper_bound(int A[],int n,int target)
 
 /**********************************************************************/
 
+int searchInsert(int A[],int n,int target)
+{
+    if (A[n-1] < target)
+        return n;
+    int low = 0,high = n-1;
+    while (low <= high)
+    {
+        int mid = low + (high-low>>1);
+        if (A[mid] == target)
+            return mid;
+        else if (A[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return low;
+}
+
+/**********************************************************************/
+
+int search_rotate(int A[],int n,int target)
+{
+    // no duplicates
+    int low = 0,high = n;
+    while (low != high)
+    {
+        int mid = low + (high-low>>1);
+        if (A[mid] == target)
+            return mid;
+        if (A[mid] >= A[low])   //left order
+        {
+            if (target < A[mid] && target >= A[low])
+                high = mid;
+            else
+                low = mid + 1;
+        }
+        else                    //right order
+        {
+            if (target > A[mid] && target <= A[high-1])
+                low = mid + 1;
+            else
+                high = mid;
+        }
+    }
+    return -1;
+}
+
+bool search_rotate(int A[],int n,int target)
+{
+    // has duplicates
+    int low = 0,high = n-1;
+    while (low <= high)
+    {
+        int mid = low + (high-low>>1);
+        if (A[mid] == target)
+            return true;
+        if (A[mid] < A[low])        //right order
+        {
+            if (target > A[mid] && target <= A[high])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        else if (A[mid] > A[low])   //left order
+        {
+            if (target >= A[low] && target < A[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        else
+            low++;
+    }
+    return false;
+}
+
+/**********************************************************************/
 
 
