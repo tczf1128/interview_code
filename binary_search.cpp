3ea2158a788050f0b13fc6ad6afbe3957d0d289b
tczf <tczf1128@gmail.com>
2014-09-04 21:51:11 +0800

    Add lower and upper bound

--- binary_search.cpp
+++ binary_search.cpp
@@ -6,13 +6,12 @@ int binary_search(int A[],int n,int target)
     while(low <= high)          // another way: low < high
     {
         int mid = low + (high-low >> 1);
-        if(A[mid] == target){
+        if (A[mid] == target)
             return mid;
-        } else if(A[mid] < target){
+        else if (A[mid] < target)
             low = mid + 1;
-        } else{
+        else
             high = mid - 1;     // another way: high = mid
-        }
     }
     return -1;
 }
@@ -30,5 +29,40 @@ int binary_search(int a[],int low,int high,int target){
 }
 
 /**********************************************************************/
+
+int find_lower_bound(int A[],int n,int target)
+{
+    int low = 0,high = n-1;
+    while (low <= high)
+    {
+        int mid = low + (high-low>>1);
+        if (A[mid] >= target)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    if (A[low] == target)
+        return low;
+    return -1;
+}
+
+int find_upper_bound(int A[],int n,int target)
+{
+    int low = 0,high = n-1;
+    while (low <= high)
+    {
+        int mid = low + (high-low>>1);
+        if(A[mid] <= target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    if (A[high] == target)
+        return high;
+    return -1;
+}
+
+/**********************************************************************/
+
 
 
