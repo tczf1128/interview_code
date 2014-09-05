baa1c060a07e199e65c8880dee7089745fc1bcf7
tczf <tczf1128@gmail.com>
2014-09-05 18:28:50 +0800

    Add insertion_sort

--- sort.cpp
+++ sort.cpp
@@ -1,2 +1,38 @@
 /*********************** sort algorithms *************************/
 
+void insertion_sort(int A[],const int start,const int end)
+{
+    int i,j,tmp;
+
+    for(i = start+1;i < end;i++)
+    {
+        tmp = A[i];
+        for(j = i-1;j >= start && tmp < A[j];j--)
+            A[j+1] = A[j];
+        A[j+1] = tmp;
+    }
+}
+/**********************************************************************/
+
+void binary_insertion_sort(int A[],const int start,const int end)
+{
+    int i,j;
+
+    for(i = start+1;i < end;i++)
+    {
+        int tmp = A[i];
+        int left = start;
+        int right = i-1;
+        while (left <= right)
+        {
+            int mid = left + (right-left>>1);
+            if (tmp < A[mid])
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        for(j = i-1;j >= left;j--)
+            A[j+1] = A[j];
+        A[left] = tmp;
+    }
+}
